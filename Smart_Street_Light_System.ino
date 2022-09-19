32085d5f91c2b21b30447956c916f04392ab9056
Vamshi Saini <103812507+VAMSAINI@users.noreply.github.com>
2022-09-19 17:27:21 +0530

    Add files via upload

---
+++ Smart_Street_Light_System.ino
@@ -0,0 +1,15 @@
+const int trigpin=9; const int echopin=10; const int ledpin=13; const int ldrpin=A0; long duration;
+int distance;
+int safedistance; void setup() {
+pinMode(trigpin,OUTPUT); pinMode(echopin,INPUT); pinMode(ledpin,OUTPUT); pinMode(ldrpin,INPUT);
+}
+void loop() { Serial.begin(9600); digitalWrite(trigpin,LOW); delayMicroseconds(2); digitalWrite(trigpin,HIGH); delayMicroseconds(10); digitalWrite(trigpin,LOW);
+duration=pulseIn(echopin,HIGH); distance=duration*0.034/2; safedistance=distance;
+int light=analogRead(ldrpin); if(light<=400){
+if(safedistance<=50){ digitalWrite(ledpin,HIGH); delay(5000);
+}
+else if(safedistance>50){ digitalWrite(ledpin,LOW); delay(10);
+}
+}
+delay(200);
+}
